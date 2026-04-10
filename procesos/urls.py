1dd9cbc65d89ed4d4cc51b9ccc83bef995d7ff86
Erick Fernando Ponce Chamorro <eponcec@levapan.com.ec>
2026-04-10 14:12:35 -0500

    Plantilla Orden Producto Avanze

--- procesos/urls.py
+++ procesos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='home'),
+    path('orden/', views.production_order_product, name='order_product')
 ]
--- procesos/views.py
+++ procesos/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'procesos/home.html')
-# Create your views here.
+#  Base template call
+def index(request):
+    return render(request, 'index.html')
+
+# Callout of the production order template for the product
+
+def production_order_product(request):
+    operaciones = [
+        {"nombre": "Enfundado de flan"},
+        {"nombre": "Empaque manual"},
+    ]
+    filas = range(10)
+    return render(request, 'process/production_order_product.html', {
+        'operaciones': operaciones,
+        'filas': filas,
+    })
