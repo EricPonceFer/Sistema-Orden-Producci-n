1e559c8c857fe8a1736d9d61e1ddbee102137db3
Erick Fernando Ponce Chamorro <eponcec@levapan.com.ec>
2026-04-10 09:40:48 -0500

    Creación de Componente SideBar para la navegación de plantillas

--- procesos/static/js/sidebar.js
+++ procesos/static/js/sidebar.js
@@ -1,14 +1,36 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    const toggleBtn = document.querySelector('.bottom button[onclick="toggleSidebar()"]');
-    sidebar.classList.toggle("collapsed");
-    if (sidebar.classList.contains("collapsed")) {
-        toggleBtn.innerHTML = "➡";
-    } else {
-        toggleBtn.innerHTML = "⬅";
-    }
+const sidebar = document.querySelector('.sidebar');
+const sidebartoggleBtn = document.querySelectorAll('.sidebar-toggle');
+const themeToggleBtn = document.querySelector('.theme-toggle');
+const themeIcon = themeToggleBtn.querySelector('.theme-icon');
+
+// Uptade the them icon based on the current theme and sidebar state
+const updateThemeIcon = () => {
+    const isDark = document.body.classList.contains('dark-theme');
+    themeIcon.textContent = sidebar.classList.contains('collapsed') ?
+    (isDark ? 'light_mode' : 'dark_mode') : "dark_mode";
 }
 
-function toggleTheme() {
-    document.body.classList.toggle("light");
+// Apply dark theme if saved or system prefers
+const savedTheme = localStorage.getItem('theme');
+const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const shouldUseDarkTheme = savedTheme === 'dark' || (!savedTheme && systemPrefersDark);
+
+document.body.classList.toggle('dark-theme', shouldUseDarkTheme);
+updateThemeIcon();
+
+sidebartoggleBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        sidebar.classList.toggle('collapsed');
+        updateThemeIcon();
+    });
+});
+
+themeToggleBtn.addEventListener('click', () => {
+    const isDark = document.body.classList.toggle('dark-theme');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+    updateThemeIcon();
+});
+
+if (window.innerWidth > 768) {
+    sidebar.classList.add('collapsed');
 }
