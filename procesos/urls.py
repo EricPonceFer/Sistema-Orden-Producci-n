e1193fea4bdd63baaa2daefa1530c0da053327a7
Erick Fernando Ponce Chamorro <eponcec@levapan.com.ec>
2026-04-14 14:09:16 -0500

    Plantilla realizado en html de reporte de producción

--- procesos/urls.py
+++ procesos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('orden/', views.production_order_product, name='order_product')
+    path('orden/', views.production_order_product, name='order_product'),
+    path('reporte/', views.production_report, name='production_report')
 ]
--- procesos/views.py
+++ procesos/views.py
@@ -12,7 +12,24 @@ def production_order_product(request):
         {"nombre": "Empaque manual"},
     ]
     filas = range(10)
+    movimiento_planta = [
+        {"movimiento": "ENTRADA POR PROCESO"},
+        {"movimiento": "SALIDA PARA"},
+        {"movimiento": "SALIDA PARA"},
+        {"movimiento": "SALIDA PARA"}
+    ]
     return render(request, 'process/production_order_product.html', {
         'operaciones': operaciones,
         'filas': filas,
+        'movimiento_planta': movimiento_planta
+    })
+
+def production_report(request):
+    control_material = [
+        {"material": "Leche", "cantidad": 100, "unidad": "litros"},
+        {"material": "Azúcar", "cantidad": 50, "unidad": "kg"},
+        {"material": "Gelatina", "cantidad": 20, "unidad": "kg"},
+    ]
+    return render(request, 'process/production_report.html', {
+        'control_material': control_material
     })
